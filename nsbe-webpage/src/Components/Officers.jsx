1b54a4da9554e749fd26f65c24baa4b2c7eac67f
Kevin Uduji <Kevinuduji@gmail.com>
2024-07-18 02:07:11 -0500

    Change in role name

--- nsbe-webpage/src/Components/Officers.jsx
+++ nsbe-webpage/src/Components/Officers.jsx
@@ -41,7 +41,7 @@ const officers = [
     {
         id: "Communications Zone",
         name: 'Kevin Uduji',
-        role: 'Co-Founder / CEO',
+        role: 'Telecommunications Coordinator',
         imageUrl:
           'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
