b23b0f6fa9a75e9c2009109fff17dc7ffbe3e2ed
Kevin Uduji <texasnsbetelecommunications@gmail.com>
2024-07-18 03:22:20 -0500

    Made Website so Far
    Made all the initial installations, pages, components, etc.
    
    Co-Authored-By: Kevin Uduji <118309007+kevinuduji@users.noreply.github.com>

--- nsbe-webpage/src/Components/Officers.jsx
+++ nsbe-webpage/src/Components/Officers.jsx
@@ -47,8 +47,8 @@ const officers = [
       },
       {
         id: "Communications Zone",
-        name: 'Leslie Alexander',
-        role: 'Co-Founder / CEO',
+        name: 'Angel Achar',
+        role: 'Public Relation Coordinator',
         imageUrl:
           'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
