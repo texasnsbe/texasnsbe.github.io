77277b0c3922b989f4edc4412c37e90a7fcd930c
Kevin Uduji <texasnsbetelecommunications@gmail.com>
2024-07-21 18:55:40 -0500

    Quick Fixes - @kevinuduji
    Small removals
    
    Co-Authored-By: Kevin Uduji <118309007+kevinuduji@users.noreply.github.com>

--- nsbe-webpage/src/Components/AboutDetails.jsx
+++ nsbe-webpage/src/Components/AboutDetails.jsx
@@ -22,12 +22,12 @@ export default function AboutDetails() {
                                     className="h-full w-full object-cover object-center "
                                 />
                             </div>
-                            <div className="h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-auto">
-                                <h1 className='text-6xl headerstyle font-black pb-10'>About Us</h1>
+                            <div className="h-64 overflow-hidden sm:h-80 lg:order-last lg:h-auto">
+                                <h1 className='ml-1 text-6xl headerstyle font-black pb-10'>About Us</h1>
                                 <p className='text-lg tracking-wide font-medium'>
-                                    <span className='text-2xl inline-block border-l-8 border-indigo-800 pl-2 font-semibold'>O</span>ur mission is to increase the number of culturally responsible Black engineers excelling academically, succeeding professionally, and positively impacting the community. .We offer resources for academic excellence, professional development, leadership, and mentorship. Through study groups, career fairs, social events, and community service projects, we create an inclusive environment for students to thrive and connect.
+                                    <span className='text-2xl inline-block border-l-8 border-indigo-800 pl-2 font-semibold'>O</span>ur mission is to increase the number of culturally responsible Black engineers excelling academically, succeeding professionally, and positively impacting the community. We offer resources for academic excellence, professional development, leadership, and mentorship. Through study groups, career fairs, social events, and community service projects, we create an inclusive environment for students to thrive and connect.
                                 </p>
-                                <Link href="https://give.utexas.edu/?menu=OGPENCS&solicit=ESL&response=DJ0ESL=ENDS&comments=National%20Society%20of%20Black%20Engineers%20%28NSBE%29%20Account:%2030-2116-8093" class="mt-10 block w-fit rounded-md bg-indigo-600 px-16 py-3 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 buttondarkbg">Fund Our Mission</Link>
+                                <Link href="https://give.utexas.edu/?menu=OGPENCS&solicit=ESL&response=DJ0ESL=ENDS&comments=National%20Society%20of%20Black%20Engineers%20%28NSBE%29%20Account:%2030-2116-8093" class="ml-1 mt-10 block w-fit rounded-md bg-indigo-600 px-16 py-3 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 buttondarkbg">Fund Our Mission</Link>
                             </div>
                         </div>
                     </div>
