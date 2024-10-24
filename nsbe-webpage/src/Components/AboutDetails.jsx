d01b227ec00a66be8c0dd29f13abe0469bfc4a68
Kevin Uduji <kevinuduji@gmail.com>
2024-10-24 12:29:58 -0500

    FIx about background

--- nsbe-webpage/src/Components/AboutDetails.jsx
+++ nsbe-webpage/src/Components/AboutDetails.jsx
@@ -11,7 +11,7 @@ const image = [
 ]
 export default function AboutDetails() {
     return (
-        <div>
+        <div className="purple-bg">
             <div className="purple-bg pt-44">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className=" mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
