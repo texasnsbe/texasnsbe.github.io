b88c91b167064161bfb73f9deea0ad1b6d7e5e73
Kevin Uduji <kevinuduji@gmail.com>
2024-11-12 18:02:26 -0600

    Update HornRaiser Link

--- nsbe-webpage/src/Components/HornsRaiser.jsx
+++ nsbe-webpage/src/Components/HornsRaiser.jsx
@@ -1,4 +1,4 @@
-import slackimage from "../assets/hornraiserimage.png";
+import slackimage from "../assets/hornraise.png";
 
 export default function HornsRaiser() {
   return (
@@ -6,10 +6,10 @@ export default function HornsRaiser() {
       <div className="flex justify-center py-[5vh]">
         <div className="flex flex-col">
           <p className="miniheader sm:text-xl font-semibold tracking-wider  text-black">
-            Help Contribute!
+            Join Fundraiser!
           </p>
           <h2 className="subheaderstyle text-gray-900 transition-all hover:scale-105 duration-300 ease-out mb-3">
-            Join HornRaiser
+            Donate Here
           </h2>
           <a
             href="https://hornraiser.utexas.edu/project/44046?clid=ZO2UL49IVFQ39XP6eB9Ck7&utm_campaign=amb_share&utm_medium=plain&utm_source=scalefunder&utm_content=ixggnkvgvw8jzxc0xikvv38"
