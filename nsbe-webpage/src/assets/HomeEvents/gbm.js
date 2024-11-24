127143076e3efa278f1c52d24ef8cbd9a985e934
Kevin Uduji <kevinuduji@gmail.com>
2024-11-24 01:32:59 -0600

    Update gbm.js

--- nsbe-webpage/src/assets/HomeEvents/gbm.js
+++ nsbe-webpage/src/assets/HomeEvents/gbm.js
@@ -1,33 +1,33 @@
-import gbm1 from '../HomeEvents/GBMHeader/5gbm.png'
-import gbm2 from '../HomeEvents/GBMHeader/4gbm.png'
+import gbm6 from '../HomeEvents/GBMHeader/6gbm.png'
+import gbm5 from '../HomeEvents/GBMHeader/5gbm.png'
+import gbm4 from '../HomeEvents/GBMHeader/4gbm.png'
 import gbm3 from '../HomeEvents/GBMHeader/3gbm.png'
 
 const gbmposts = [
-    // {
-    //   title: 'General Body Meeting 6',
-    //   href: 'https://docs.google.com/presentation/d/1nr1DpKFWhFM6cdPKQrBR-yeNxEYuBzpG86QVajzKd-Q/edit#slide=id.p',
-    //   // TODO LINK
-    //   date: 'Nov 19, 2024',
-    //   slideImg: gbm1,
-    // },
     {
-      title: 'General Body Meeting 5',
+      title: '#6',
+      href: 'https://docs.google.com/presentation/d/1ubqzSne9Io8f3CkfkcRvcqjlndPJjT_Ze9Mu8xJb1eo/edit?usp=sharing',
+      date: 'Nov 19, 2024',
+      slideImg: gbm6,
+    },
+    {
+      title: '#5',
       href: 'https://docs.google.com/presentation/d/1LXkfUnqKXMIA-EHkNqHFzni426iyFZJK6lpxyop_UaQ/edit?usp=sharing',
       date: 'Nov 5, 2024',
-      slideImg: gbm1,
+      slideImg: gbm5,
     },
     {
-      title: 'General Body Meeting 4',
+      title: '#4',
       href: 'https://docs.google.com/presentation/d/1c-p88OhVpKGEtRKMUk9OBJCO3UXKGXqWwLj0XAY3vl0/edit?usp=sharing',
       date: 'Oct 22, 2024',
-      slideImg: gbm2,
+      slideImg: gbm4,
     },
-    {
-      title: 'General Body Meeting 3',
-      href: 'https://docs.google.com/presentation/d/1nr1DpKFWhFM6cdPKQrBR-yeNxEYuBzpG86QVajzKd-Q/edit?usp=sharing',
-      date: 'Oct 8, 2024',
-      slideImg: gbm3,
-    },
+    // {
+    //   title: 'General Body Meeting 3',
+    //   href: 'https://docs.google.com/presentation/d/1nr1DpKFWhFM6cdPKQrBR-yeNxEYuBzpG86QVajzKd-Q/edit?usp=sharing',
+    //   date: 'Oct 8, 2024',
+    //   slideImg: gbm3,
+    // },
     
     // More posts...
   ]
