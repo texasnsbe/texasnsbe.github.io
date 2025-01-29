e5ca42d7dee1c97488c76de36f603a638effa73a
Kevin Uduji <kevinuduji@gmail.com>
2025-01-29 09:47:45 -0600

    New Semester, New Site

--- nsbe-webpage/src/Components/HornsRaiser.jsx
+++ nsbe-webpage/src/Components/HornsRaiser.jsx
@@ -1,4 +1,4 @@
-import slackimage from "../assets/hornraise.png";
+import image from "../assets/eweek.jpg";
 
 export default function HornsRaiser() {
   return (
@@ -6,18 +6,18 @@ export default function HornsRaiser() {
       <div className="flex justify-center py-[5vh]">
         <div className="flex flex-col">
           <p className="miniheader sm:text-xl font-semibold tracking-wider  text-black">
-            Support NSBE! | Closes Dec 4.
+            Build Community w/ Engineers
           </p>
           <h2 className="subheaderstyle text-gray-900 transition-all hover:scale-105 duration-300 ease-out mb-3">
-            Donate Here
+            E-Week is Coming!
           </h2>
           <a
-            href="https://hornraiser.utexas.edu/project/44046?clid=ZO2UL49IVFQ39XP6eB9Ck7&utm_campaign=amb_share&utm_medium=plain&utm_source=scalefunder&utm_content=ixggnkvgvw8jzxc0xikvv38"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
           >
             <img
-              src={slackimage}
+              src={image}
               className="object-cover h-[73vh] w-[60vw] max-lg:h-[40vh] max-md:h-[35vh] max-sm:h-[35vh] max-sm:w-[90vw] darkbg transition-all hover:scale-105 duration-300 ease-out"
             />
           </a>
--- nsbe-webpage/src/assets/HomeEvents/gbm.js
+++ nsbe-webpage/src/assets/HomeEvents/gbm.js
@@ -1,27 +1,28 @@
 import gbm6 from '../HomeEvents/GBMHeader/6gbm.png'
 import gbm5 from '../HomeEvents/GBMHeader/5gbm.png'
-import gbm4 from '../HomeEvents/GBMHeader/4gbm.png'
+import gbm7 from '../HomeEvents/GBMHeader/7gbm.png'
 import gbm3 from '../HomeEvents/GBMHeader/3gbm.png'
 
 const gbmposts = [
     {
-      title: '#6',
-      href: 'https://docs.google.com/presentation/d/1ubqzSne9Io8f3CkfkcRvcqjlndPJjT_Ze9Mu8xJb1eo/edit?usp=sharing',
-      date: 'Nov 19, 2024',
-      slideImg: gbm6,
+      title: '#7',
+      href: 'https://docs.google.com/presentation/d/1nogjDUdVo4v77MWYXBlM-7GLpqwIj3IHkMaQCYo6bmc/edit?usp=sharing',
+      date: 'Jan 28, 2025',
+      slideImg: gbm7,
     },
-    {
-      title: '#5',
-      href: 'https://docs.google.com/presentation/d/1LXkfUnqKXMIA-EHkNqHFzni426iyFZJK6lpxyop_UaQ/edit?usp=sharing',
-      date: 'Nov 5, 2024',
-      slideImg: gbm5,
-    },
-    {
-      title: '#4',
-      href: 'https://docs.google.com/presentation/d/1c-p88OhVpKGEtRKMUk9OBJCO3UXKGXqWwLj0XAY3vl0/edit?usp=sharing',
-      date: 'Oct 22, 2024',
-      slideImg: gbm4,
-    },
+    // {
+    //   title: '#6',
+    //   href: 'https://docs.google.com/presentation/d/1ubqzSne9Io8f3CkfkcRvcqjlndPJjT_Ze9Mu8xJb1eo/edit?usp=sharing',
+    //   date: 'Nov 19, 2024',
+    //   slideImg: gbm6,
+    // },
+    // {
+    //   title: '#5',
+    //   href: 'https://docs.google.com/presentation/d/1LXkfUnqKXMIA-EHkNqHFzni426iyFZJK6lpxyop_UaQ/edit?usp=sharing',
+    //   date: 'Nov 5, 2024',
+    //   slideImg: gbm5,
+    // },
+    
     // {
     //   title: 'General Body Meeting 3',
     //   href: 'https://docs.google.com/presentation/d/1nr1DpKFWhFM6cdPKQrBR-yeNxEYuBzpG86QVajzKd-Q/edit?usp=sharing',
